e626b6f4dabca9b1771b898c876cc133b3ff05b0
Sachin Semlety <sachin.semlety@cmss.in>
2017-06-20 16:27:35 +0530

    added repo

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp')
+, concat = require("gulp-concat"),
+del = require('del'),
+uglify = require('gulp-uglify'),
+pump = require('pump');
+
+
+var defaultTasks = ['clean', 'compress'];
+
+
+gulp.task('compress', ['clean'],function (cb) {
+  pump([
+        gulp.src('lib/*.js').pipe(concat('dist.min.js')),
+        uglify(),
+        gulp.dest('dist')
+    ],
+    cb
+  );
+});
+
+
+gulp.task('clean', function (cb) {
+    return del(['dist/'], cb);
+});
+
+gulp.task('default', defaultTasks);
